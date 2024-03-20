f0bcbf27ac03ea2a5b5116f32f44cf57799b69f9
s-salcedo <163323692+s-salcedo@users.noreply.github.com>
2024-03-20 16:29:49 +0100

    "Primer commit en neatBeans"

---
+++ src/unidad4_githubholamundonb/Unidad4_GitHubHolaMundoNB.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unidad4_githubholamundonb;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class Unidad4_GitHubHolaMundoNB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
